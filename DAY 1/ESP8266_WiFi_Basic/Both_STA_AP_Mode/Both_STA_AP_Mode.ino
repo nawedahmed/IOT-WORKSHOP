8b0b35aea84f391e6d4c56daab59fa10084f073f
Nawed Ahmad <mdnawedahmad78692@gmail.com>
2025-12-25 21:24:28 +0545

    final code

--- DAY 1/ESP8266_WiFi_Basic/Both_STA_AP_Mode/Both_STA_AP_Mode.ino
+++ DAY 1/ESP8266_WiFi_Basic/Both_STA_AP_Mode/Both_STA_AP_Mode.ino
@@ -1,66 +1,66 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-#define LED D1
+#define LED 2  // Built-in LED (D4 on some boards)
 
-// Router credentials (STA)
-const char* ssid = "realme 12x 5G";
-const char* password = "12345678mm";
+const char* ssid = "realme 12x 5G";       // Your router SSID
+const char* password = "12345678mm";      // Your router password
 
-// AP credentials
-const char* ap_ssid = "ESP8266_AP";
-const char* ap_pass = "12345678";
+const char* ap_ssid = "ESP8266_AP";       // ESP AP SSID
+const char* ap_pass = "12345678";         // ESP AP password
 
 ESP8266WebServer server(80);
 
 void handleRoot() {
   server.send(200, "text/html",
-    "<h2>ESP8266 AP + STA</h2>"
-    "<a href='/on'><button>LED ON</button></a>"
-    "<a href='/off'><button>LED OFF</button></a>"
+    "<!DOCTYPE html>"
+    "<html>"
+    "<head>"
+      "<meta name='viewport' content='width=device-width, initial-scale=1'>"
+      "<style>"
+        "body { display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh; font-family: Arial; }"
+        "button { width: 120px; height: 60px; font-size: 20px; margin: 10px; border-radius: 10px; }"
+      "</style>"
+    "</head>"
+    "<body>"
+      "<h2>ESP8266 AP + STA</h2>"
+      "<a href='/on'><button>LED ON</button></a>"
+      "<a href='/off'><button>LED OFF</button></a>"
+    "</body>"
+    "</html>"
   );
 }
 
+
 void setup() {
-  Serial.begin(9600);
-
+  Serial.begin(115200);
   pinMode(LED, OUTPUT);
-  digitalWrite(LED, LOW);
+  digitalWrite(LED, HIGH);  // LED OFF initially (built-in LED is active LOW)
 
+  // Dual mode
   WiFi.mode(WIFI_AP_STA);
-
-  // STA mode
   WiFi.begin(ssid, password);
-
-  // AP mode
   WiFi.softAP(ap_ssid, ap_pass);
 
-  // -------- SERIAL INFO (IMPORTANT FOR STUDENTS) --------
   Serial.println("\nESP8266 Dual Mode Started");
-
-  Serial.print("AP IP Address: ");
-  Serial.println(WiFi.softAPIP());   // AP IP
+  Serial.print("AP IP Address: "); Serial.println(WiFi.softAPIP());
 
   Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
   Serial.println("\nConnected to Router");
-  Serial.print("STA IP Address: ");
-  Serial.println(WiFi.localIP());    // STA IP
-  // -----------------------------------------------------
+  Serial.print("STA IP Address: "); Serial.println(WiFi.localIP());
 
+  // Web server routes
   server.on("/", handleRoot);
-
   server.on("/on", []() {
-    digitalWrite(LED, HIGH);
+    digitalWrite(LED, LOW);    // Turn ON (built-in LED is active LOW)
     server.send(200, "text/plain", "LED ON");
   });
-
   server.on("/off", []() {
-    digitalWrite(LED, LOW);
+    digitalWrite(LED, HIGH);   // Turn OFF
     server.send(200, "text/plain", "LED OFF");
   });
 
@@ -71,72 +71,3 @@ void setup() {
 void loop() {
   server.handleClient();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ------ Simplified code but nothing show in serial monitor but work properly
-
-
-//#include <ESP8266WiFi.h>
-//#include <ESP8266WebServer.h>
-//
-//#define LED D1
-//
-//// Router credentials (STA)
-//const char* ssid = "realme 12x 5G";
-//const char* password = "12345678mm";
-//
-//// AP credentials
-//const char* ap_ssid = "ESP8266_AP";
-//const char* ap_pass = "12345678";
-//
-//ESP8266WebServer server(80);
-//
-//void handleRoot() {
-//  server.send(200, "text/html",
-//    "<h2>ESP8266 AP + STA</h2>"
-//    "<a href='/on'><button>LED ON</button></a>"
-//    "<a href='/off'><button>LED OFF</button></a>"
-//  );
-//}
-//
-//void setup() {
-//  pinMode(LED, OUTPUT);
-//  digitalWrite(LED, LOW);
-//
-//  WiFi.mode(WIFI_AP_STA);
-//
-//  // STA mode
-//  WiFi.begin(ssid, password);
-//
-//  // AP mode
-//  WiFi.softAP(ap_ssid, ap_pass);
-//
-//  server.on("/", handleRoot);
-//  server.on("/on", []() {
-//    digitalWrite(LED, HIGH);
-//    server.send(200, "text/plain", "LED ON");
-//  });
-//  server.on("/off", []() {
-//    digitalWrite(LED, LOW);
-//    server.send(200, "text/plain", "LED OFF");
-//  });
-//
-//  server.begin();
-//}
-//
-//void loop() {
-//  server.handleClient();
-//}
