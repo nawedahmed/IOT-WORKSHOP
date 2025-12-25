7366797c92b33f0d56b51695dc3e8fc03178fa3c
Nawed Ahmad <mdnawedahmad78692@gmail.com>
2025-12-25 20:37:49 +0545

    solve welcome code

--- DAY 1/ESP8266_WiFi_Basic/welcome_code/welcome_code.ino
+++ DAY 1/ESP8266_WiFi_Basic/welcome_code/welcome_code.ino
@@ -3,50 +3,52 @@
  *  Note: esp8266 and laptop or mobile whatever you are using should connected to the same wifi to see the welcome message.
  */
 
-
-#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>       // For Wi-Fi connection
+#include <ESP8266WebServer.h>  // For HTTP server
 
 const char* ssid = "realme 12x 5G";
 const char* password = "12345678mm";
 
-
-WiFiServer server(80); // HTTP server on port 80
+ESP8266WebServer server(80);  // HTTP server on port 80
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password); // Connect to Wi-Fi
-  Serial.println("Connecting to Wi-Fi...");
 
-  while (WiFi.status() != WL_CONNECTED) {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to Wi-Fi...");
+
+  unsigned long startTime = millis();
+
+  // Timeout after 15 sec
+  while (WiFi.status() != WL_CONNECTED && millis() - startTime < 15000) {
     delay(500);
     Serial.print(".");
   }
 
-  Serial.println("\nConnected!");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
-  
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nConnected!");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
 
-  server.begin(); // Start HTTP server
+    // Define root URL handler
+    server.on("/", []() {
+      server.send(200, "text/html",
+        "<html><body style='text-align:center;'>"
+        "<h1>Welcome to IoT Workshop<br>Electrical Club, Pulchowk Campus</h1>"
+        "</body></html>"
+      );
+    });
+
+    server.begin();
+    Serial.println("HTTP Server started");
+  } else {
+    Serial.println("\nWi-Fi connection failed!");
+  }
 }
 
 void loop() {
-  WiFiClient client = server.available(); // Check for incoming client
-
-  if (client) {
-    String request = client.readStringUntil('\r'); // Read request
-    client.flush();
-
-    // HTML content
-    String html = "<!DOCTYPE html><html><head><title>IoT Workshop</title></head>"
-                  "<body style='display:flex;justify-content:center;align-items:center;height:100vh;'>"
-                  "<h1><b>Welcome to IoT Workshop by Electrical Club at Pulchowk Campus </b></h1>"
-                  "</body></html>";
-
-    // Send HTTP response
-    client.print("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n");
-    client.print(html);
-
-    client.stop(); // Close connection
+  if (WiFi.status() == WL_CONNECTED) {
+    server.handleClient();  // Handle incoming clients
   }
 }
